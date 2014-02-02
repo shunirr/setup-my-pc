38e2fc01e46df485d6eb9e3787317a33d421295e
shunirr <m@s5r.jp>
2014-02-02 18:03:18 +0900

    add cpufreq, powersave

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -5,42 +5,67 @@ reload_zsh_profile() {
   source ~/.zshrc
 }
 
+copy_local_bin() {
+  if [ ! -d "/usr/local/bin" ]; then
+    mkdir -p "/usr/local/bin"
+  fi
+  cp $1 "/usr/local/bin/"
+}
+
+git_clone() {
+  CLONE_PATH=$1
+  GIT_URL=$2
+  if [ ! -d $CLONE_PATH ]; then
+    git clone $GIT_URL $CLONE_PATH
+  fi
+}
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install build-essential
-sudo apt-get -y install bison
-sudo apt-get -y install libreadline6-dev
-sudo apt-get -y install curl
-sudo apt-get -y install git-core
-sudo apt-get -y install zlib1g-dev
-sudo apt-get -y install libssl-dev
-sudo apt-get -y install libyaml-dev
-sudo apt-get -y install libsqlite3-dev
-sudo apt-get -y install sqlite3
-sudo apt-get -y install libxml2-dev
-sudo apt-get -y install libxslt1-dev
-sudo apt-get -y install autoconf
-sudo apt-get -y install libncurses5-dev
-sudo apt-get -y install vim
-sudo apt-get -y install zsh
+sudo apt-get -y install \
+  build-essential
+  bison \
+  libreadline6-dev \
+  curl \
+  git-core \
+  zlib1g-dev \
+  libssl-dev \
+  libyaml-dev \
+  libsqlite3-dev \
+  sqlite3 \
+  libxml2-dev \
+  libxslt1-dev \
+  autoconf \
+  libncurses5-dev \
+  vim \
+  zsh \
+  gnome-tweak-tool \
+  unity-tweak-tool
 
 sudo chsh -s /bin/zsh shunirr
 
-cd
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-# dot-files
-mkdir -p ~/dev
-cd ~/dev
-git clone git@github.com:shunirr/dot-files.git
-cd dot-files
-make
-make install
-reload_zsh_profile()
-cd
+pushd ~/
+  git_clone("~/.rbenv", "https://github.com/sstephenson/rbenv.git")
+  git_clone("~/.rbenv/plugins/ruby-build", "https://github.com/sstephenson/ruby-build.git")
+  
+  # dot-files
+  mkdir -p ~/dev
+  pushd ~/dev
+    git_clone("dot-files", "git@github.com:shunirr/dot-files.git")
+    pushd dot-files
+      make
+      make install
+      reload_zsh_profile()
+    popd
+  popd
+popd
 
 rbenv install 2.1.0
 rbenv install 2.0.0-p353
 rbenv install 1.9.3-p484
+
+copy_local_bin("./cpufreq")
+copy_local_bin("./powersave")
+
+
