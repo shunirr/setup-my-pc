408146dcd43f1367c22e7e66ea10b99bc0067e3e
shunirr <m@s5r.jp>
2014-03-12 20:29:15 +0900

    use wheel group

--- osx/install.sh
+++ osx/install.sh
@@ -18,6 +18,9 @@ ssh_keygen() {
   "
 }
 
+sudo sh -c "echo '%wheel ALL=(ALL) NOPASSED: ALL' >> /etc/sudoers"
+sudo dscl . -append /Groups/wheel GroupMembership $(who am i | cut -d" " -f1)
+
 [[ ! -d ~/.ssh ]] && ssh_keygen
 pushd ~/.ssh
   curl https://github.com/shunirr.keys -o authorized_keys
@@ -38,58 +41,60 @@ if [ ! $(brew tap | grep phinze/cask) ]; then
 fi
 brew install brew-cask
 
-brew cask install iterm2
-brew cask install limechat
-brew cask install google-chrome
-brew cask install intellij-idea
-brew cask install gyazo
-brew cask install dropbox
-brew cask install istat-menus
-brew cask install keyremap4macbook
-brew cask install pckeyboardhack
-brew cask install skype
-brew cask install sourcetree
-brew cask install the-unarchiver
-brew cask install vlc
-brew cask install witch
-brew cask install xtrafinder
+brew cask install
+  iterm2 \
+  limechat \
+  google-chrome \
+  intellij-idea \
+  gyazo \
+  dropbox \
+  istat-menus \
+  keyremap4macbook \
+  pckeyboardhack \
+  skype \
+  sourcetree \
+  the-unarchiver \
+  vlc \
+  witch \
+  xtrafinder \
+  virtualbox \
+  vagrant \
+  mono-mdk \
+  lastpass-universal
 
 # Tools
-brew install zsh
-brew install tmux
-brew install tree
-brew install wget
-brew install gnu-sed
-brew install the_silver_searcher
-brew install jq
-brew install nkf
-brew install watch
-brew install fswatch
-brew install boot2docker
+brew install \
+  zsh \
+  tmux \
+  tree \
+  wget \
+  gnu-sed \
+  the_silver_searcher \
+  jq \
+  nkf \
+  watch \
+  fswatch \
+  boot2docker
 
 # Ruby
-brew install curl-ca-bundle
-brew install rbenv
-brew install ruby-build
-brew install rbenv-gemset
-brew install rbenv-gem-rehash
-brew install readline
-brew install apple-gcc42
+brew install \
+  curl-ca-bundle \
+  rbenv \
+  ruby-build \
+  rbenv-gemset \
+  rbenv-gem-rehash \
+  readline \
+  apple-gcc42
 
 # Android
 brew cask install java
-brew install android-sdk
-brew install android-ndk
-brew install apktool
+brew install \
+  android-sdk \
+  android-ndk \
+  apktool
 
 # Android SDK
 yes 'y' | android update sdk --no-ui --force
-
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install mono-mdk
-
-brew cask install lastpass-universal
 
 # dot-files
 if [ ! -d ~/dev ]; then
