a13f2adf37aff2e53ac502537eb590023d4a4a2a
shunirr <m@s5r.jp>
2020-12-16 12:46:55 +0900

    Change zoomus -> zoom

--- osx.sh
+++ osx.sh
@@ -193,7 +193,7 @@ brew_cask_install visual-studio-code
 brew_cask_install visual-studio # Mono/Xamarin
 brew_cask_install notable
 brew_cask_install istat-menus
-brew_cask_install zoomus
+brew_cask_install zoom
 brew_cask_install microsoft-office
 brew_cask_install google-backup-and-sync
 
