0b4d9b5a10c9fd1c6df7be0a47df0e96f5fdca61
shunirr <m@s5r.jp>
2022-06-09 16:41:09 +0900

    google drive

--- osx.sh
+++ osx.sh
@@ -252,6 +252,7 @@ brew_cask_install "/Applications/Google Chrome.app" google-chrome
 brew_cask_install "/Applications/Slack.app" slack
 brew_cask_install "/Applications/Utilities/Adobe Creative Cloud/ACC/Creative Cloud.app" adobe-creative-cloud
 brew_cask_install "/Applications/Kindle.app" kindle
+brew_cask_install "/Applications/Google Drive.app" google-drive
 
 
 mas_install 539883307 # LINE
