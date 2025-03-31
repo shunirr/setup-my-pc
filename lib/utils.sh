cc4b9b792617d4b0a3a0953962b4bd27ba938fcb
shunirr <git@s5r.jp>
2025-03-31 21:21:21 +0900

    update

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+info() {
+  echo -e "\e[35m[INFO]\e[m $1"
+}
+
 wait_process() {
+  info "Waiting for '$1'"
+  set +e
   sleep 5
   while true; do
     sleep 1
@@ -8,9 +14,11 @@ wait_process() {
       break
     fi
   done
+  set -e
 }
 
 ssh_keygen() {
+  info "Generating SSH key"
   SSH_KEY_PATH="$HOME/.ssh/id_ed25519"
   if [ $# -eq 1 ]; then
     SSH_KEY_PATH="${SSH_KEY_PATH}_$1"
@@ -21,21 +29,23 @@ ssh_keygen() {
 }
 
 add_sudoers() {
+  info "Adding sudoers"
   if [ ! -d "/etc/sudoers.d" ] || [ ! -f "/etc/sudoers.d/config" ]; then
-    sudo mkdir -p /etc/sudoers.d
-    echo '%wheel ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/config
-    sudo chmod 440 /etc/sudoers.d/*
+    sudo mkdir -p "/etc/sudoers.d"
+    echo '%wheel ALL=(ALL) NOPASSWD: ALL' | sudo tee "/etc/sudoers.d/config"
+    sudo chmod 440 "/etc/sudoers.d/*"
   fi
 }
 
 join_wheel_group() {
-  USERNAME="$(who am i | cut -d" " -f1)"
-  if ! dscl . -read /Groups/wheel | grep -q "$USERNAME"; then
-    sudo dscl . -append /Groups/wheel GroupMembership "$USERNAME"
+  info "Joining wheel group"
+  if ! dscl . -read "/Groups/wheel" | grep -q "$USER"; then
+    sudo dscl . -append "/Groups/wheel" "GroupMembership" "$USER"
   fi
 }
 
 install_command_line_developer_tools() {
+  info "Installing Command Line Developer Tools"
   if [ ! -d "/Library/Developer/CommandLineTools" ]; then
     /bin/bash -c "xcode-select --install" &
     wait_process "Command Line Developer Tools"
@@ -43,35 +53,50 @@ install_command_line_developer_tools() {
 }
 
 install_homebrew() {
+  info "Installing Homebrew"
   if ! type brew; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-    if [ "$(uname)" = "Darwin" ] && [ "$(uname -m)" = "arm64" ]; then
-      eval "$(/opt/homebrew/bin/brew shellenv)"
+    /bin/bash -c "$(curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh')"
+    if [ "$(uname)" = "Darwin" ]; then
+      if  [ "$(uname -m)" = "arm64" ]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+      else
+        eval "$(/usr/local/bin/brew shellenv)"
+      fi
     fi
   fi
 }
 
 homebrew_init() {
   install_homebrew
-  brew update
-  brew upgrade
+
+  info "Update Homebrew"
+  brew "update"
+
+  info "Upgrade Homebrew"
+  brew "upgrade"
 }
 
+BREW_INSTALLED=""
+
 brew_install() {
-  if [ ! -d "$(brew --prefix)/Cellar/$1" ]; then
+  info "Installing $1"
+  if [ -z "$BREW_INSTALLED" ]; then
+    BREW_INSTALLED="$(brew info --installed --json | jq '.[].full_name')"
+  fi
+  if ! echo "$BREW_INSTALLED" | grep -q \""$1"\"; then
     brew install "$1"
   fi
 }
 
 brew_cask_install() {
-  if [ ! -d "$1" ] && [ ! -d "$(brew --prefix)/Caskroom/$2" ]; then
-    set +e
-    brew install --cask "$2"
-    set -e
+  info "Installing $1"
+  if [ ! -d "$(brew --prefix)/Caskroom/$1" ]; then
+    brew install --cask -f "$1"
   fi
 }
 
 mas_install() {
+  info "Installing $1"
   if [ -d "$(brew --prefix)/Caskroom/$1" ]; then
     brew uninstall "$1"
   fi
@@ -80,23 +105,40 @@ mas_install() {
   fi
 }
 
+gem_install() {
+  info "Installing $1"
+  if ! which "$1" | grep -q "$1"; then
+    gem install "$1"
+  fi
+}
+
+npm_install() {
+  info "Installing $1"
+  if ! which "$1" | grep -q "$1"; then
+    npm install -g "$1"
+  fi
+}
+
 install_hackgen() {
+  info "Installing HackGen"
   if [ -z "$(find "$HOME/Library/Fonts" -name 'HackGen*.ttf')" ]; then
-    brew tap homebrew/cask-fonts
-    brew_install font-hackgen
-    brew_install font-hackgen-nerd
+    brew tap "homebrew/cask-fonts"
+    brew_install "font-hackgen"
+    brew_install "font-hackgen-nerd"
   fi
 }
 
 install_rosetta2() {
+  info  "Installing Rosetta 2"
   if [ "$(uname)" = "Darwin" ] && [ "$(uname -m)" = "arm64" ]; then
     softwareupdate --install-rosetta --agree-to-license
   fi
 }
 
 uninstall_asdf() {
+  info "Uninstalling asdf"
   if [ -d "$(brew --prefix)/Cellar/asdf" ]; then
-    brew uninstall --force asdf
+    brew uninstall --force "asdf"
     brew autoremove
   fi
   if [ -f "$HOME/.asdfrc" ]; then
@@ -108,4 +150,48 @@ uninstall_asdf() {
   if [ -d "$HOME/.asdf" ]; then
     rm -rf "$HOME/.asdf"
   fi
+}
+
+uninstall_java8() {
+  info "Uninstalling Java 8"
+  if [ -d "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin" ]; then
+    sudo rm -rf "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin"
+  fi
+  if [ -d "/Library/PreferencesPanes/JavaControlPanel.prefPane" ]; then
+    sudo rm -rf "/Library/PreferencesPanes/JavaControlPanel.prefPane"
+  fi
+  if [ -d "$HOME/Library/Application Support/Java" ]; then
+    sudo rm -rf "$HOME/Library/Application Support/Java"
+  fi
+}
+
+install_fenv() {
+  info "Installing fenv"
+  if [ ! -f "$HOME/.fenv/bin/fenv" ]; then
+    curl -fsSL "https://fenv-install.jerry.company" | bash
+    # shellcheck disable=SC2086
+    "$HOME/.fenv/bin/fenv" "init"
+  fi
+}
+
+change_shell() {
+  info "Changing shell to $1"
+  if ! grep <"/etc/shells" -q "$1"; then
+    echo "$1" | sudo tee -a "/etc/shells"
+  fi
+  if [ "$(dscl localhost -read "Local/Default/Users/$USER" UserShell | cut -d' ' -f2)" != "$1" ]; then
+    chsh -s "$1"
+  fi
+}
+
+mise_plugin_add() {
+  info "Adding mise plugin $1"
+  if ! mise plugin list | grep -q "$1"; then
+    mise plugin add "$1"
+  fi
+}
+
+mise_install() {
+  info "Installing apps by mise"
+  mise install
 }
--- osx.sh
+++ osx.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
 source ./lib/utils.sh
-
-RUBY_VERSION="3.3.6"
-PYTHON_VERSION="3.13.1"
-PYTHON_POETRY_VERSION="1.8.5"
-NODE_VERSION="22.12.0"
-JAVA_VERSION="adoptopenjdk-11.0.25+9"
-KOTLIN_VERSION="2.1.0"
-GOLANG_VERSION="1.23.4"
-DENO_VERSION="2.1.4"
-
-########
 
 if [ $# -eq 1 ] && [ "$1" = "p" ]; then
   IS_PERSONAL="true"
@@ -25,49 +14,31 @@ fi
 
 add_sudoers
 join_wheel_group
-
 ssh_keygen
 
 install_rosetta2
-
 install_command_line_developer_tools
+
+# Copy dot-files
+cp -R "dot-files/." "$HOME"
 
 homebrew_init
 
-brew_install mas
-
-# mas_install 497799835 # Xcode
-# sudo xcodebuild -license accept
-
-brew_install shellcheck
-if ! shellcheck "$0" "./lib/utils.sh"; then
-  echo "Please fix shellcheck's problems"
-  exit 1
-fi
-
-brew_install git
-brew_install git-lfs
-brew_install tmux
-brew_install wget
-brew_install the_silver_searcher
-brew_install jq
-brew_install ccache
-brew_install cmake
-brew_install pkg-config
-
-# Copy dot-files
-cp -v -R dot-files/. "$HOME"
+brew_install "git"
+brew_install "git-lfs"
+brew_install "tmux"
+brew_install "wget"
+brew_install "the_silver_searcher"
+brew_install "jq"
+brew_install "ccache"
+brew_install "cmake"
+brew_install "pkgconf"
 
 # Bash
-brew_install bash
-brew_install bash-completion
+brew_install "bash"
+brew_install "bash-completion"
 
-if ! grep <"/etc/shells" -q "$(which bash)"; then
-  which bash | sudo tee -a /etc/shells
-fi
-if [ "$(dscl localhost -read "Local/Default/Users/$USER" UserShell | cut -d' ' -f2)" != "$(which bash)" ]; then
-  chsh -s "$(which bash)"
-fi
+change_shell "$(which bash)"
 
 if [ ! -f "$HOME/.bashrc_private" ]; then
   touch "$HOME/.bashrc_private"
@@ -77,117 +48,82 @@ fi
 # shellcheck disable=SC1091
 source "$HOME/.bashrc"
 
-# uninstall asdf
 uninstall_asdf
+uninstall_java8
 
 # mise
-curl https://mise.run | sh
+if ! type mise >/dev/null 2>&1; then
+  curl "https://mise.run" | sh
+fi
 
 # Ruby
-brew_install libyaml
-mise use --global "ruby@$RUBY_VERSION"
-
-if ! which bundle | grep -q "mise"; then
-  gem install bundler
-fi
+brew_install "libyaml"
+gem_install "bundler"
 
 # Python
-mise use --global "python@$PYTHON_VERSION"
-mise plugin add poetry
-mise use --global "poetry@$PYTHON_POETRY_VERSION"
+mise_plugin_add "poetry"
 
 # Nodejs
-mise use --global "nodejs@$NODE_VERSION"
-if ! which yarn | grep -q "mise"; then
-  npm install -g yarn
-fi
-if ! which pnpm | grep -q "mise"; then
-  npm install -g pnpm
-fi
+npm_install "yarn"
+npm_install "pnpm"
 
-# Deno
-mise use --global "deno@$DENO_VERSION"
-
-# Uninstall default java8
-if [ -d "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin" ]; then
-  sudo rm -rf "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin"
-fi
-if [ -d "/Library/PreferencesPanes/JavaControlPanel.prefPane" ]; then
-  sudo rm -rf "/Library/PreferencesPanes/JavaControlPanel.prefPane"
-fi
-if [ -d "$HOME/Library/Application Support/Java" ]; then
-  sudo rm -rf "$HOME/Library/Application Support/Java"
-fi
-
-# Java
-mise use --global "java@$JAVA_VERSION"
-
-# Kotlin
-mise use --global "kotlin@$KOTLIN_VERSION"
-
-# Golang
-mise use --global "golang@$GOLANG_VERSION"
-
-# reshim
-mise reshim --force
+mise_install
 
 # Flutter
-if [ ! -f "$HOME/.fenv/bin/fenv" ]; then
-  curl -fsSL "https://fenv-install.jerry.company" | bash
-  # shellcheck disable=SC2086
-  $HOME/.fenv/bin/fenv init
-fi
+install_fenv
 
 # Android
-brew_cask_install "/Applications/Android Studio.app" android-studio
-brew_install apktool
-brew_install bundletool
-
-# Java
-brew_cask_install "/Applications/IntelliJ IDEA CE.app" intellij-idea-ce
+brew_cask_install "android-studio"
+brew_install "apktool"
+brew_install "bundletool"
 
 # Graph
-brew_install graphviz
-brew_install plantuml
+brew_install "graphviz"
+brew_install "plantuml"
 
 # Terraform
-brew_install awscli
-brew_install tfenv
+brew_install "awscli"
+brew_install "tfenv"
 
 # Xcodes
-brew_install xcodesorg/made/xcodes
-brew_cask_install "/Applications/Xcodes.app" xcodes
+brew_install "xcodesorg/made/xcodes"
+brew_cask_install "xcodes"
+info "Install Xcode by xcodes"
+xcodes install --latest
+sudo xcodebuild -license accept
 
-# Other applications
-brew_install bitwarden-cli
+brew_install "bitwarden-cli"
 
-brew_cask_install "/Applications/Karabiner-Elements.app" karabiner-elements
-brew_cask_install "/Library/Input Methods/AquaSKK.app" aquaskk
-brew_cask_install "/Applications/WezTerm.app" wezterm
-brew_cask_install "/Applications/Visual Studio Code.app" visual-studio-code
-brew_cask_install "/Applications/Microsoft Word.app" microsoft-office
-brew_cask_install "/Applications/zoom.us.app" zoom
-brew_cask_install "/Applications/Google Chrome.app" google-chrome
-brew_cask_install "/Applications/Proxyman.app" proxyman
-brew_cask_install "/Applications/Finicky.app" finicky
-brew_cask_install "/Applications/Rancher Desktop.app" rancher
-brew_cask_install "/Applications/BetterDisplay.app" betterdisplay
-brew_cask_install "/Applications/Obsidian.app" obsidian
+brew_cask_install "karabiner-elements"
+brew_cask_install "aquaskk"
+brew_cask_install "wezterm"
+brew_cask_install "visual-studio-code"
+brew_cask_install "microsoft-office"
+brew_cask_install "zoom"
+brew_cask_install "google-chrome"
+brew_cask_install "proxyman"
+brew_cask_install "finicky"
+brew_cask_install "rancher"
+brew_cask_install "betterdisplay"
+brew_cask_install "obsidian"
 
 if [ "$IS_PERSONAL" = 'true' ]; then
-  brew_cask_install "/Applications/Utilities/Adobe Creative Cloud/ACC/Creative Cloud.app" adobe-creative-cloud
-  brew_cask_install "/Applications/Claude.app" claude
-  brew_cask_install "/Applications/Cursor.app" cursor
+  brew_cask_install "adobe-creative-cloud"
+  brew_cask_install "claude"
+  brew_cask_install "cursor"
 else
   ssh_keygen "w"
-  brew_cask_install "/Applications/Firefox.app" firefox
-  brew_cask_install "/Applications/Dialpad.app" dialpad
-  brew_cask_install "/Applications/Box.app" box-drive
+  brew_cask_install "firefox"
+  brew_cask_install "dialpad"
+  brew_cask_install "box-drive"
 fi
 
-mas_install line 539883307
-mas_install slack 803453959
-mas_install the-unarchiver 425424353
+# Mac App Store
+brew_install "mas"
+
+mas_install "line"           "539883307"
+mas_install "slack"          "803453959"
+mas_install "the-unarchiver" "425424353"
 
 # Fonts
 install_hackgen
