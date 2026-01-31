3f8518b622c0af7c91ac3ec60be00151f268f155
shunirr <git@s5r.jp>
2026-01-31 17:37:53 +0900

    AquaSKK -> macSKK

--- dot-files/.config/wezterm/wezterm.lua
+++ dot-files/.config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local act = wezterm.action
 return {
   debug_key_events = true,
   use_ime = true,
+  macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL',
   font = wezterm.font('HackGen35 Console NF', { weight = 'Bold' }),
   font_size = 13.0,
   color_scheme = "Builtin Solarized Dark",
