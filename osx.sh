b79c6f2544819d79c07968d7a538c211d998420d
shunirr <git@s5r.jp>
2026-05-11 16:36:46 +0900

    update

--- osx.sh
+++ osx.sh
@@ -103,7 +103,6 @@ brew_cask_install lm-studio
 brew_cask_install codex
 brew_install claude-code
 brew install gemini-cli
-brew install copilot-cli
 
 brew_cask_install cmux
 
@@ -128,9 +127,6 @@ if [ "$IS_PERSONAL" = 'true' ]; then
   # 3D CAD
   brew_cask_install bambu-studio
   brew_cask_install autodesk-fusion
-else
-  brew_cask_install microsoft-office
-  brew_cask_install zoom
 fi
 
 # Mac App Store
@@ -140,72 +136,6 @@ mas_install 803453959 slack
 mas_install 425424353 the-unarchiver
 mas_install 1352778147 bitwarden
 mas_install 640199958 # Developer
-
-
-# VSCode Extensions
-install_vscode_extension "ms-ceintl.vscode-language-pack-ja"
-
-# Markdown
-install_vscode_extension "bierner.markdown-mermaid"
-install_vscode_extension "bpruitt-goddard.mermaid-markdown-syntax-highlighting"
-install_vscode_extension "yzhang.markdown-all-in-one"
-
-install_vscode_extension "charliermarsh.ruff"
-install_vscode_extension "chiehyu.vscode-astyle"
-install_vscode_extension "streetsidesoftware.code-spell-checker"
-
-# Flutter
-install_vscode_extension "dart-code.dart-code"
-install_vscode_extension "dart-code.flutter"
-
-# JavaScript
-install_vscode_extension "dbaeumer.vscode-eslint"
-install_vscode_extension "esbenp.prettier-vscode"
-install_vscode_extension "orta.vscode-jest"
-install_vscode_extension "svelte.svelte-vscode"
-
-# Deno
-install_vscode_extension "denoland.vscode-deno"
-
-# Git
-install_vscode_extension "eamodio.gitlens"
-install_vscode_extension "mhutchie.git-graph"
-
-install_vscode_extension "github.copilot"
-install_vscode_extension "github.copilot-chat"
-install_vscode_extension "github.vscode-github-actions"
-
-# Shell Script
-install_vscode_extension "foxundermoon.shell-format"
-install_vscode_extension "timonwong.shellcheck"
-
-# Golang
-install_vscode_extension "golang.go"
-
-# Graph
-install_vscode_extension "jebbs.plantuml"
-install_vscode_extension "tintinweb.graphviz-interactive-preview"
-
-# Android
-install_vscode_extension "mathiasfrohlich.kotlin"
-install_vscode_extension "vscjava.vscode-gradle"
-
-# Python
-install_vscode_extension "ms-python.black-formatter"
-install_vscode_extension "ms-python.debugpy"
-install_vscode_extension "ms-python.python"
-install_vscode_extension "ms-python.vscode-pylance"
-
-install_vscode_extension "hashicorp.terraform"
-
-install_vscode_extension "mechatroner.rainbow-csv"
-install_vscode_extension "ms-azuretools.vscode-docker"
-install_vscode_extension "ms-vscode-remote.remote-containers"
-install_vscode_extension "redhat.vscode-xml"
-install_vscode_extension "editorconfig.editorconfig"
-
-# neovim
-install_vscode_extension "asvetliakov.vscode-neovim"
 
 info "Upgrade all casks (excluding auto-update apps)"
 EXCLUDE_CASKS="autodesk-fusion"
